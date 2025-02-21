6610089c7a6ab51f428797d724c6b0e343692c22
Amro <0014amro@gmail.com>
2025-02-21 10:49:37 +0100

    some stuff

---
+++ MvcMovie/wwwroot/js/fetchAPI.js
@@ -0,0 +1,25 @@
+﻿const options = {
+    method: 'GET'
+};
+
+let url = 'https://randomuser.me/api'
+
+let isAlreadyFetched = sessionStorage.getItem("fetched");
+
+if (isAlreadyFetched === null) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("data");
+            console.log(data)
+            relevantInfo = {
+                Name: data.results[0].name.first,
+                Age: data.results[0].dob.age
+            }
+            localStorage.setItem("dataStore", JSON.stringify(relevantInfo));
+            sessionStorage.setItem("fetched", "true");
+        });
+        .catch (err => console.error(err));
+
+    const modelData = JSON.parse(localStorage.getItem("dataStore"));
+}
