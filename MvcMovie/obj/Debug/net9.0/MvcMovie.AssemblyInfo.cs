f85f13bde237b13812588301601a7d0617d6d1e7
Alvar Vähälummukka <alvar@lummukka.com>
2025-03-28 09:13:07 +0100

    Filtering of Tags implemented

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d599a090059386ed2edd86224178156f8dacc6e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba388d8b58df25d912fdfbd438a60d3d01a7dad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -6,6 +6,26 @@
 function AdminLogin(){
     if (document.getElementById("username").value === "Admin") {
         document.getElementById("disclaimer").style.display = "none";
-        document.getElementById("submit").style.display = "block"
+        document.getElementById("submit").style.display = "block";
     }
+}
+
+function FilterTags() {
+    var tag = document.getElementById("FilterTag").value;
+    var children = document.getElementById("productcontainer").children;
+    for (var i = 0; i < children.length; i++) {
+        children[i].style.display = "none";
+    }
+    matching = document.getElementsByClassName(tag);
+    for (var i = 0; i < matching.length; i++) {
+        matching[i].style.display = "grid";
+    }
+}
+
+function ResetFilter(){
+    var children = document.getElementById("productcontainer").children;
+    for (var i = 0; i < children.length; i++) {
+        children[i].style.display = "grid";
+    }
+    document.getElementById("FilterTag").value = "";
 }
